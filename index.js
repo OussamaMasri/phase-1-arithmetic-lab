c069ceef3b39236b7937308d30ca3462fd18fe09
Oussama Masri <oussamamasri16120@gmail.com>
2022-07-28 21:13:44 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
-// Write your code here
+let num1 = 1 , num2 = 62;
+let multiply = (num1*num2);
+
+
+let random = Math.floor(Math.random() * num2);
+
+
+let num3 = 4 , num4 = 16 ;
+let mod = num3 % num4;
+
+
+let max = Math.max(1,5,20);
